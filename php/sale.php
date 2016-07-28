2857912b6a9b1d2c24e20d1d2cbd724eef42f958
Majid <majid.razvi@sage.com>
2016-07-28 18:22:50 +0000

    functional

--- php/sale.php
+++ php/sale.php
@@ -1,12 +1,20 @@
 <?php
-
-    // first, let's get some basic requirements assigned to variables:
     
-    // developer credentials, via the dev portal
+    // ===============================
+    // SAMPLE DIRECT API REQUEST - PHP
+    // Majid Razvi
+    // Software Engineer
+    // Sage Payment Solutions
+    // July 28th, 2016
+    // Standard MIT License.
+    // ===============================
+    
+    // your developer credentials
     $client_id = "kCy6zhnC7MYN9mgA4SxaKePUdxnBrJ5W";
     $client_key = "SeN0uFZtYhqN7gsJ";
     
-    // merchant credentials -- ask us for a test account!
+    // you (or your client's) merchant credentials.
+    // grab a test account from us for development!
     $merchant_id = "417227771521";
     $merchant_key = "I5T2R2K6V1Q3";
     
@@ -18,10 +26,12 @@
     // of its timestamp header.
     $timestamp = (string)time();
     
-    // now the request data itself:
+    // setting up the request data itself
     $verb = "POST";
     $url = "https://api.sagepayments.com/bankcard/v1/charges?type=Sale";
-    $requestArray = [
+    $requestData = [
+        // this is a pretty minimalistic request...
+        // complete reference material is available on the dev portal.
         "Ecommerce" => [
             "OrderNumber" => "Invoice123",
             "Amounts" => [
@@ -33,26 +43,47 @@
             ]
         ]
     ];
-    $payload = json_encode($requestArray);
+    // convert to json for transport
+    $payload = json_encode($requestData);
 
-    // the Authorization header expects an HMAC
-    // we generate this HMAC by concatenating certain variables, 
-    // and then hashing it using our developer key.
-    
-    // concat the request's HTTP verb, target URL, request body, merchant ID,
-    // nonce, and timestamp. if you don't have a payload (eg, on a GET),
-    // just use an empty string
+    // the request is authorized via an HMAC header that we generate by
+    // concatenating certain info, and then hashing it using our client key
     $toBeHashed = $verb . $url . $payload . $merchant_id . $nonce . $timestamp;
-    
-    // http://php.net/manual/en/function.hash-hmac.php
-    $hash = hash_hmac(
-        "sha512", // use the SHA-512 algorithm
-        "",
-        $client_key, // use your private dev key to sign
-        true // php returns hexits by default; override this
+    $hmac = hash_hmac(
+        "sha512", // use the SHA-512 algorithm...
+        $toBeHashed, // ... to hash the combined string...
+        $client_key, // .. using your private dev key to sign it.
+        true // (php returns hexits by default; override this)
     );
+    // convert to base-64 for transport
+    $hmac_b64 = base64_encode($hmac);
+
+    echo $hmac_b64;
+
+    // ok, let's make the request! cURL is always an option, of course,
+    // but i find that file_get_contents is a bit more intuitive.
+    $config = [
+        "http" => [
+            "header" => [
+                "clientId: " . $client_id,
+                "merchantId: " . $merchant_id,
+                "merchantKey: " . $merchant_key,
+                "nonce: " . $nonce,
+                "timestamp: " . $timestamp,
+                "authorization: " . $hmac_b64,
+                "content-type: application/json",
+            ],
+            "method" => $verb,
+            "content" => $payload
+        ]
+    ];
+    $context = stream_context_create($config);
+    $result = file_get_contents($url, false, $context);
     
-    
-    echo base64_encode($hash);
+    if ($result === false){
+        //  error handling
+    }else{
+        print_r($result);
+    }
 
 ?>
